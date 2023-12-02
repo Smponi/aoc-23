7a641837f257f3b56e0c9841a4b84e188799e7f5
Philipp <Philipp@smponias.de>
2023-12-02 16:37:18 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { it.getFirstAndLastDigitAsInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { it.replaceNameOfDigitsWithNumber().getFirstAndLastDigitAsInt() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+    check(input.size==1000)
+
     part1(input).println()
+
+
+    val part2Test = part2(readInput("Day01Test"))
+    check(part2Test == 281)
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,33 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Utility function for day 1
+ */
+fun String.getFirstAndLastDigitAsInt(): Int {
+    return this.filter { it.isDigit() }.getFirstAndLast().toInt()
+}
+
+fun String.getFirstAndLast() = "${first()}${last()}"
+
+/**
+ * Junk way to add the int representation of the word.
+ * E.g. If the string contains "one" it will be replaced with "one1one"
+ */
+fun String.replaceNameOfDigitsWithNumber(): String {
+    println("Before switching: $this")
+    val result =
+        this
+            .replace("eight", "eight8eight")
+            .replace("two", "two2two")
+            .replace("one", "one1one")
+            .replace("four", "four4four")
+            .replace("five", "five5five")
+            .replace("six", "six6six")
+            .replace("seven", "seven7seven")
+            .replace("nine", "nine9nine")
+            .replace("three", "three3three")
+    "After switching: $result".println()
+    return result
+}
